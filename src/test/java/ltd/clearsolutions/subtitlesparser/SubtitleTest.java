97d695d8e72fd60182c3ddaca272df9744640296
Saifferela <Saifferela@gmail.com>
2021-12-30 16:13:39 +0200

    Added tests.

--- src/test/java/ltd/clearsolutions/subtitlesparser/SubtitleTest.java
+++ src/test/java/ltd/clearsolutions/subtitlesparser/SubtitleTest.java
@@ -3,6 +3,7 @@ package ltd.clearsolutions.subtitlesparser;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SubtitleTest {
 
@@ -13,5 +14,14 @@ class SubtitleTest {
         subtitle.setTime(time);
         assertThat(49048).isEqualTo(subtitle.getStartTime());
         assertThat(51801).isEqualTo(subtitle.getEndTime());
+    }
+
+    @Test
+    void setTime_IncorrectTimeString_ExceptionIncorrectTimeData() {
+        String time = "00:00:49 --> 00:00:51,801";
+        Subtitle subtitle = new Subtitle();
+
+        Subtitle.IncorrectTimeData thrown = assertThrows(Subtitle.IncorrectTimeData.class, () -> subtitle.setTime(time));
+        assertThat(thrown).hasMessageEndingWith("The time type is incorrect");
     }
 }
--- src/test/java/ltd/clearsolutions/subtitlesparser/SubtitlesParserTest.java
+++ src/test/java/ltd/clearsolutions/subtitlesparser/SubtitlesParserTest.java
@@ -3,7 +3,6 @@ package ltd.clearsolutions.subtitlesparser;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -11,14 +10,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class SubtitlesParserTest {
 
 
-    String path = "src/test/resources/Squid.Game.S01E09.1080p.NF.WEB-DL.DDP5.1.Atmos.x264-UGM.srt";
+    String path = "src/test/resources/CorrectSubs.srt";
     File file = new File(path);
+    SubtitlesParser subtitlesParser = new SubtitlesParser();
 
 
     @Test
     void subtitlesParser_FirstPieceOfTheFile_FirstItemText() {
-        SubtitlesParser subtitlesParser = new SubtitlesParser();
-        subtitlesParser.subtitlesParser(file);
         Subtitle expected = subtitlesParser.subtitlesParser(file).get(0);
 
         assertThat(1).isEqualTo(expected.number);
@@ -29,8 +27,6 @@ class SubtitlesParserTest {
 
     @Test
     void subtitlesParser_LastPieceOfTheFile_LastItemText() {
-        SubtitlesParser subtitlesParser = new SubtitlesParser();
-        subtitlesParser.subtitlesParser(file);
         int size = subtitlesParser.subtitlesParser(file).size() - 1;
         Subtitle expected = subtitlesParser.subtitlesParser(file).get(size);
 
@@ -38,5 +34,20 @@ class SubtitlesParserTest {
         assertThat(3239485).isEqualTo(expected.startTime);
         assertThat(3244407).isEqualTo(expected.endTime);
         assertThat(List.of("Subtitle translation by: Eun-sook Yoon")).isEqualTo(expected.text);
+    }
+
+    @Test
+    void subtitlesParser_FileWithHTML_LastItemText() {
+        String path = "src/test/resources/HTMLSubs.srt";
+        File file = new File(path);
+        SubtitlesParser subtitlesParser = new SubtitlesParser();
+
+        int size = subtitlesParser.subtitlesParser(file).size() - 1;
+        Subtitle expected = subtitlesParser.subtitlesParser(file).get(size);
+
+        assertThat(663).isEqualTo(expected.number);
+        assertThat(3060989).isEqualTo(expected.startTime);
+        assertThat(3063992).isEqualTo(expected.endTime);
+        assertThat(List.of("[CLOSING THEME MUSIC]")).isEqualTo(expected.text);
     }
 }
